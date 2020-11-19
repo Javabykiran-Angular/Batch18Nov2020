b2a3d02054fe0ae6a7dc925112140426d4bbd6d0
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-11-19 09:25:14 +0530

    Day2

---
+++ Typescript/Day2/demo1.js
@@ -0,0 +1,13 @@
+//comment
+/*
+in core java
+system.out.println("hvsjdhvb "+temp);
+*/
+console.log("Hello World!!!!");
+console.log("Welcome to Typescript");
+var a;
+var a$b;
+//Data Types
+// number(int,double,float),string("z",'sumit'),boolean(true,flase),void,any
+//Misec data type undefined , null
+var a1;
