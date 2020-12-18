71a7a3ae9f72a97fd1c75dc2762e7d8b4281d2d9
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-12-18 09:44:09 +0530

    Day14

---
+++ Angular/Day14/app-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FollowerComponent } from './follower/follower.component';
+import { HomeComponent } from './home/home.component';
+import { MyfollowerComponent } from './myfollower/myfollower.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { QueryparamComponent } from './queryparam/queryparam.component';
+
+
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'Page1',component:Page1Component
+  },
+  {
+    path:'Page2',
+    component:Page2Component
+    
+  },
+  {
+    path:'Page3',
+    component:Page3Component
+  },
+  {
+    path:'Follower',
+    component:FollowerComponent
+  },
+  {
+    path:'MyFollower',
+    component:MyfollowerComponent
+  },
+  {
+    path:'QueryParam',
+    component:QueryparamComponent
+  },
+  {
+    path:'**',
+    component:NotfoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Angular/Day14/queryparam/queryparam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-queryparam',
+  templateUrl: './queryparam.component.html',
+  styleUrls: ['./queryparam.component.css']
+})
+export class QueryparamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
